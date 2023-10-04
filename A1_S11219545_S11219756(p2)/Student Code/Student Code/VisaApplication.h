56502487e7fdeb54f6706a845334aabe1c9c9d51
Ivan-alfred <146906713+Ivan-alfred@users.noreply.github.com>
2023-10-04 15:40:43 +1200

    Update VisaApplication.h

--- A1_S11219545_S11219756(p2)/Student Code/Student Code/VisaApplication.h
+++ A1_S11219545_S11219756(p2)/Student Code/Student Code/VisaApplication.h
@@ -15,7 +15,20 @@ class visa_application : public data {
 		string status;
 		string result;
 	public:
-		
+		void set_visa_type(string visa_type);
+		string get_visa_type();
+		void set_invoice_no(int invoice_no);
+		int get_invoice_no();
+		void set_surname(string surname);
+		string get_surname();
+		void set_first_name(string first_name);
+		string get_first_name();
+		void set_contact(int contact);
+		int get_contact();
+		void set_status(string status);
+		string get_status();
+		void set_result(string result);
+		string get_result();
 
 
 		void print();
