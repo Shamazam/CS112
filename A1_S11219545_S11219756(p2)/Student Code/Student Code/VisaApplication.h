db11faab55c723953dfd6f8a11395083344af57e
Shamazam <shamalfiji@gmail.com>
2023-10-04 15:28:52 +1200

    changes

--- A1_S11219545_S11219756(p2)/Student Code/Student Code/VisaApplication.h
+++ A1_S11219545_S11219756(p2)/Student Code/Student Code/VisaApplication.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <iostream>
+#include "Data.h"
+using namespace std;
+
+class visa_application : public data {
+
+	private:
+		string visa_type;
+		int invoice_no;
+		string surname;
+		string first_name;
+		int contact;
+		string status;
+		string result;
+	public:
+		
+
+
+		void print();
+
+
+};
